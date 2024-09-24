c6d3264549422d8bf16566e4d173b1c9443c7da4
viktorbirgiss <viktor.birgisson@siminn.is>
2024-09-24 14:58:01 +0000

    added preview

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,8 @@
 import type { Metadata } from "next";
 import localFont from "next/font/local";
 import "./globals.css";
+import { PrismicPreview } from "@prismicio/next";
+import { repositoryName } from "@/prismicio";
 
 const geistSans = localFont({
   src: "./fonts/GeistVF.woff",
@@ -29,6 +31,7 @@ export default function RootLayout({
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
         {children}
+        <PrismicPreview repositoryName={repositoryName} />
       </body>
     </html>
   );
